37ef20d0b09d36c02654c9336c6137c4b5966c47
Joseph Abad <jabad3@gatech.edu>
2017-01-19 16:18:23 -0500

    testing some updates

--- train.py
+++ train.py
@@ -54,6 +54,83 @@ for index, row in data_frame.iterrows():
 # release the main data_frame from memory
 data_frame = None
 
+
+
+
+
+
+
+####
+# load data file
+data_frame = pd.read_csv('curves/driving_log.csv', usecols=[0, 1, 2, 3])
+
+# shuffle the data
+data_frame = data_frame.sample(frac=1).reset_index(drop=True)
+print(data_frame)
+
+x_images = []
+y_labels = []
+for index, row in data_frame.iterrows():
+    LEFT_RIGHT_CORRECTION_CONSTANT = 0.20
+    
+    left_image_path = row['left']
+    center_image_path = row['center']
+    right_image_path = row['right']
+    center_steering_value = row['steering']
+    
+    left_image = load_image_values("curves/" + left_image_path.strip())
+    center_image = load_image_values("curves/" + center_image_path.strip())
+    right_image = load_image_values("curves/" + right_image_path.strip())
+    
+    # add images
+    x_images.append(left_image)
+    y_labels.append(center_steering_value+LEFT_RIGHT_CORRECTION_CONSTANT)
+    
+    x_images.append(center_image)
+    y_labels.append(center_steering_value)
+    
+    x_images.append(right_image)
+    y_labels.append(center_steering_value-LEFT_RIGHT_CORRECTION_CONSTANT)
+
+    # mini augment
+    left_flipped_image = cv2.flip(left_image, 1)
+    center_flipped_image = cv2.flip(center_image, 1)
+    right_flipped_image = cv2.flip(right_image, 1)
+    center_flipped_steering_value = center_steering_value*-1
+    
+    x_images.append(left_flipped_image)
+    y_labels.append(center_flipped_steering_value+LEFT_RIGHT_CORRECTION_CONSTANT)
+    
+    x_images.append(center_flipped_image)
+    y_labels.append(center_flipped_steering_value)
+    
+    x_images.append(right_flipped_image)
+    y_labels.append(center_flipped_steering_value-LEFT_RIGHT_CORRECTION_CONSTANT)
+
+    
+# release the main data_frame from memory
+data_frame = None
+####
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 x_images = np.asarray(x_images)
 y_labels = np.asarray(y_labels)
 

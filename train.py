b1f08c784b488d600d2fd0cdae22e06ff3dcc533
Joseph Abad <jabad3@gatech.edu>
2017-01-21 17:21:10 -0500

    trying to augment the tricky turn data

--- train.py
+++ train.py
@@ -255,29 +255,54 @@ data_frame = pd.read_csv('the_tricky_turn/driving_log.csv', usecols=[0, 1, 2, 3]
 
 # shuffle the data
 data_frame = data_frame.sample(frac=1).reset_index(drop=True)
-#print(data_frame)
-
-#x_images = []
-#y_labels = []
-for index, row in data_frame.iterrows(): 
+for index, row in data_frame.iterrows():
+    
+    HARD = 0.10
+    BOOSTER = 0.05
+    
+    #left_image_path = row['left']
     center_image_path = row['center']
+    right_image_path = row['right']
     center_steering_value = row['steering']
-    center_image = load_image_values("the_tricky_turn/" + center_image_path.strip())
+    
+    #left_image = load_image_values("data/" + left_image_path.strip())
+    center_image = load_image_values("data/" + center_image_path.strip())
+    right_image = load_image_values("data/" + right_image_path.strip())
     
     # add images
+    #x_images.append(left_image)
+    #y_labels.append(center_steering_value-SOFT)
+    
     x_images.append(center_image)
     y_labels.append(center_steering_value)
+    
+    x_images.append(center_image)
+    y_labels.append(center_steering_value-BOOSTER)
+    
+    x_images.append(right_image)
+    y_labels.append(center_steering_value-HARD)
 
     # mini augment
+    #left_flipped_image = cv2.flip(left_image, 1)
     center_flipped_image = cv2.flip(center_image, 1)
+    #right_flipped_image = cv2.flip(right_image, 1)
     center_flipped_steering_value = center_steering_value*-1
+    
+    #x_images.append(left_flipped_image)
+    #y_labels.append(center_flipped_steering_value+HARD)
     
     x_images.append(center_flipped_image)
     y_labels.append(center_flipped_steering_value)
     
+    x_images.append(center_flipped_image)
+    y_labels.append(center_flipped_steering_value+BOOSTER)
+    
+    #x_images.append(right_flipped_image)
+    #y_labels.append(center_flipped_steering_value-SOFT)
+
+    
 # release the main data_frame from memory
 data_frame = None
-####
 
 
 

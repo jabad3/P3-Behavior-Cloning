8f980266389bcbae7957cf78ab53308be218d54d
Joe <carnd@ip-172-31-48-183.ec2.internal>
2017-01-21 18:19:08 +0000

    lowering the constants, still doesnt work

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ data_frame = data_frame.sample(frac=1).reset_index(drop=True)
 x_images = []
 y_labels = []
 for index, row in data_frame.iterrows():
-    LEFT_RIGHT_CORRECTION_CONSTANT = 0.20
+    LEFT_RIGHT_CORRECTION_CONSTANT = 0.05
     
     left_image_path = row['left']
     center_image_path = row['center']
@@ -26,14 +26,14 @@ for index, row in data_frame.iterrows():
     right_image = load_image_values("data/" + right_image_path.strip())
     
     # add images
-    x_images.append(left_image)
-    y_labels.append(center_steering_value+LEFT_RIGHT_CORRECTION_CONSTANT)
+    #x_images.append(left_image)
+    #y_labels.append(center_steering_value+LEFT_RIGHT_CORRECTION_CONSTANT)
     
     x_images.append(center_image)
     y_labels.append(center_steering_value)
     
-    x_images.append(right_image)
-    y_labels.append(center_steering_value-LEFT_RIGHT_CORRECTION_CONSTANT)
+    #x_images.append(right_image)
+    #y_labels.append(center_steering_value-LEFT_RIGHT_CORRECTION_CONSTANT)
 
     # mini augment
     left_flipped_image = cv2.flip(left_image, 1)
@@ -41,14 +41,14 @@ for index, row in data_frame.iterrows():
     right_flipped_image = cv2.flip(right_image, 1)
     center_flipped_steering_value = center_steering_value*-1
     
-    x_images.append(left_flipped_image)
-    y_labels.append(center_flipped_steering_value+LEFT_RIGHT_CORRECTION_CONSTANT)
+    #x_images.append(left_flipped_image)
+    #y_labels.append(center_flipped_steering_value+LEFT_RIGHT_CORRECTION_CONSTANT)
     
     x_images.append(center_flipped_image)
     y_labels.append(center_flipped_steering_value)
     
-    x_images.append(right_flipped_image)
-    y_labels.append(center_flipped_steering_value-LEFT_RIGHT_CORRECTION_CONSTANT)
+    #x_images.append(right_flipped_image)
+    #y_labels.append(center_flipped_steering_value-LEFT_RIGHT_CORRECTION_CONSTANT)
 
     
 # release the main data_frame from memory
@@ -104,7 +104,7 @@ data_frame = pd.read_csv('edge_recovery/driving_log.csv', usecols=[0, 1, 2, 3])
 
 # shuffle the data
 data_frame = data_frame.sample(frac=1).reset_index(drop=True)
-print(data_frame)
+#print(data_frame)
 
 x_images = []
 y_labels = []
@@ -156,8 +156,8 @@ data_frame = None
 
 
 
-HARD_CONSTANT = 0.35
-SOFT_CONSTANT = 0.25
+HARD_CONSTANT = 0.11
+SOFT_CONSTANT = 0.06
 
 
 ##################################################
@@ -289,7 +289,7 @@ y_labels = np.asarray(y_labels)
 model = get_model()
 #model.fit(training_generator, validation_data=validation_data_generator, samples_per_epoch=samples_per_epoch, nb_epoch=3, nb_val_samples=3000)
 #model.fit(x_train_data, Y_train_data, batch_size=128, nb_epoch=2, validation_split=0.2)
-model.fit(x_images, y_labels, batch_size=128, nb_epoch=150, validation_split=0.2)
+model.fit(x_images, y_labels, batch_size=128, nb_epoch=120, validation_split=0.2)
 
 print("Saving model.")
 model.save_weights('model.h5')
